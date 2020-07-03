325e8703852a91e24bb8bce02ea558b955f3139e
Bluesmoke <43018850+hibenca@users.noreply.github.com>
2020-07-03 11:49:21 -0500

    Fix JS bug and add number of people

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 function calculator() {
     let billAmount = document.getElementById('billAmount').value;
-    document.getElementById('results1').innerHTML = (billAmount * .1);
-    document.getElementById('results1').innerHTML = (billAmount * .2);
-    document.getElementById('results1').innerHTML = (billAmount * .5);
+    document.getElementById('results1').innerHTML = ('$' + billAmount * .1);
+    document.getElementById('results2').innerHTML = ('$' + billAmount * .2);
+    document.getElementById('results3').innerHTML = ('$' + billAmount * .5);
 };
