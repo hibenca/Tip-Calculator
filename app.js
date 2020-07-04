c0be048f2f7d2dc0ad42c3b07e440e756956af44
hibenca <hiben06@gmail.com>
2020-07-04 11:58:11 -0500

    Update app.js & styles.css

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 function calculator() {
     let billAmount = document.getElementById('billAmount').value;
-    document.getElementById('results1').innerHTML = ('$' + billAmount * .1);
-    document.getElementById('results2').innerHTML = ('$' + billAmount * .2);
-    document.getElementById('results3').innerHTML = ('$' + billAmount * .5);
+    let people = document.getElementById('people').value;
+    document.getElementById('results1').innerHTML = ('$' + billAmount * .1 / people);
+    document.getElementById('results2').innerHTML = ('$' + billAmount * .2 / people);
+    document.getElementById('results3').innerHTML = ('$' + billAmount * .5 / people);
 };
