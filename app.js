9b89cb70f59fa241c5b29d9b97c1e77db07af9a2
Bluesmoke <43018850+hibenca@users.noreply.github.com>
2020-07-06 18:19:10 -0500

    Add styling

--- app.js
+++ app.js
@@ -6,4 +6,5 @@ function calculator() {
     document.getElementById('results1').innerHTML = ('$' + billAmount * .1 / people);
     document.getElementById('results2').innerHTML = ('$' + billAmount * .2 / people);
     document.getElementById('results3').innerHTML = ('$' + billAmount * .5 / people);
+    document.getElementById('totalTip').style.display = "block";
 };
