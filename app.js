71f80f355703e496eb0c6866c028ee966dd0aa5d
Bluesmoke <43018850+hibenca@users.noreply.github.com>
2020-07-07 07:28:42 -0500

    Add flow control & toFixed

--- app.js
+++ app.js
@@ -3,8 +3,15 @@
 function calculator() {
     let billAmount = document.getElementById('billAmount').value;
     let people = document.getElementById('people').value;
-    document.getElementById('results1').innerHTML = ('$' + billAmount * .1 / people);
-    document.getElementById('results2').innerHTML = ('$' + billAmount * .2 / people);
-    document.getElementById('results3').innerHTML = ('$' + billAmount * .5 / people);
-    document.getElementById('totalTip').style.display = "block";
+
+
+    if (billAmount <= 0) {
+        alert("Please fill out the amount");
+    } else {
+        document.getElementById('results1').innerHTML = ('$' + (billAmount * .1 / people).toFixed(2));
+        document.getElementById('results2').innerHTML = ('$' + (billAmount * .2 / people).toFixed(2));
+        document.getElementById('results3').innerHTML = ('$' + (billAmount * .5 / people).toFixed(2));
+        document.getElementById('totalTip').style.display = "block";
+    };
+
 };
